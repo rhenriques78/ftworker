e4ec01425b89409584fc3b5f10921fb563332f84
Ricardo Henriques <rhenriques@imaginarycloud.com>
2024-05-27 16:59:45 +0100

    First push

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,46 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { randomBytes } from "node:crypto"
+
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request) {
+
+		// sample for calling a node library
+		randomBytes(256, (err, buffer) => {
+			const token = buffer.toString("hex");
+			if (err) throw err;
+			console.log(token);
+		});
+
+		class AttributeRewriter {
+			attributeName: string;
+
+			constructor(attributeName: string) {
+				this.attributeName = attributeName;
+			}
+			element(element: HTMLElement) {
+				const attribute = element.getAttribute(this.attributeName);
+				if (attribute && attribute.startsWith("/")) {
+					element.setAttribute(this.attributeName, "/home" + attribute);
+				}
+			}
+		}
+
+		const urlFormatted = `https://web.bankee.online/${request.url.split("/home")[1]}`;
+
+		const response = await fetch(urlFormatted, {
+			cf: { cacheTtl: 3600 },
+			redirect: "follow", // default behaviour
+			method: request.method,
+		});
+
+		const rewriter = new HTMLRewriter().on(
+			"a",
+			new AttributeRewriter("href")
+		);
+
+		return rewriter.transform(response);
 	},
 };
+
